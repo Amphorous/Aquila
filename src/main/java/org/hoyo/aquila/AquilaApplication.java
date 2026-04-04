5eee60bb972482341cbbabd9191bedefba444e25
Asim <161995961+MdMusthaqAsim@users.noreply.github.com>
2026-04-05 04:42:59 +0530

    the gateway, that is aquila

---
+++ src/main/java/org/hoyo/aquila/AquilaApplication.java
@@ -0,0 +1,13 @@
+package org.hoyo.aquila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquilaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AquilaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/hoyo/aquila/AquilaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hoyo.aquila;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquilaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
